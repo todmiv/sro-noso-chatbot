5f514c0e9fe99463b8608761581172e85417e192
todmiv <115306605+todmiv@users.noreply.github.com>
2025-07-12 14:53:33 +0300

    Update start.py

--- app/bot/handlers/start.py
+++ app/bot/handlers/start.py
@@ -1,1 +1,10 @@
+from aiogram import Router, types
 
+router = Router()
+
+@router.message(commands={"start", "старт"})
+async def cmd_start(message: types.Message) -> None:
+    await message.answer(
+        "Добро пожаловать в чат-бот СРО НОСО!\n"
+        "Введите /help для списка команд."
+    )
