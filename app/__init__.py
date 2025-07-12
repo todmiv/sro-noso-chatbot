fbf58d940906784e1c923e71c7ea9ec304afbf1e
todmiv <115306605+todmiv@users.noreply.github.com>
2025-07-12 14:51:35 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,7 @@
+"""Top-level package for SRO NOSO Chatbot."""
+from asyncio import set_event_loop_policy, WindowsSelectorEventLoopPolicy
+import sys
 
+# Windows-совместимость для asyncio <3.12
+if sys.platform.startswith("win"):
+    set_event_loop_policy(WindowsSelectorEventLoopPolicy())
