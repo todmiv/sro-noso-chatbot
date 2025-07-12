4ed6e6006c1bcbbe2ff0593f38d51d84c54dc77d
todmiv <115306605+todmiv@users.noreply.github.com>
2025-07-12 15:03:42 +0300

    Update logging_config.py

--- app/utils/logging_config.py
+++ app/utils/logging_config.py
@@ -1,1 +1,9 @@
+import logging
+import structlog
+from config.settings import config
 
+def setup_logging() -> None:
+    logging.basicConfig(level=config.log_level)
+    structlog.configure(
+        wrapper_class=structlog.make_filtering_bound_logger(config.log_level)
+    )
