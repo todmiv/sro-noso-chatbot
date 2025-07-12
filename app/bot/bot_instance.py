ee5be45c61b0b2ea81fd8edc9485cd43fde1f5c5
todmiv <115306605+todmiv@users.noreply.github.com>
2025-07-12 14:52:23 +0300

    Update bot_instance.py

--- app/bot/bot_instance.py
+++ app/bot/bot_instance.py
@@ -1,1 +1,11 @@
+"""Factory функции для создания Bot и Dispatcher."""
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage
+from config.settings import config
 
+def create_bot() -> Bot:
+    return Bot(token=config.bot.token)
+
+def create_dispatcher() -> Dispatcher:
+    storage = RedisStorage.from_url(config.database.redis_url)
+    return Dispatcher(storage=storage)
