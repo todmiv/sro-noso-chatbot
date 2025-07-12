30038a69d08f1e3821c140485423da7aa492c0ca
todmiv <115306605+todmiv@users.noreply.github.com>
2025-07-12 14:55:22 +0300

    Update logging_middleware.py

--- app/bot/middleware/logging_middleware.py
+++ app/bot/middleware/logging_middleware.py
@@ -1,1 +1,15 @@
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+from typing import Callable, Awaitable, Any
 
+class LoggingMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: dict[str, Any],
+    ) -> Any:
+        from structlog import get_logger
+        logger = get_logger()
+        logger.info("incoming_update", update=event)
+        return await handler(event, data)
