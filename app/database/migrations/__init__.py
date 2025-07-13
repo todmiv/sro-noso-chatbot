f084035410733b5b490c32a70fe69c2d0afa3ef2
todmiv <115306605+todmiv@users.noreply.github.com>
2025-07-13 14:30:10 +0300

    Update __init__.py

--- app/database/migrations/__init__.py
+++ app/database/migrations/__init__.py
@@ -1,1 +1,12 @@
+"""Миграции базы данных."""
 
+# Порядок применения миграций
+MIGRATION_ORDER = [
+    "001_initial",
+    "002_add_feedback", 
+    "003_add_indexing"
+]
+
+def get_migration_order():
+    """Возвращает порядок применения миграций."""
+    return MIGRATION_ORDER
