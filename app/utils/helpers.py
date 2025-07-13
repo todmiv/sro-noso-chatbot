ca00dcea3876fe8292eae45718500a8bd688fdff
todmiv <115306605+todmiv@users.noreply.github.com>
2025-07-13 10:29:35 +0300

    Update helpers.py

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,1 +1,16 @@
+from typing import Iterable, List, TypeVar
 
+T = TypeVar("T")
+
+
+def chunked(iterable: Iterable[T], n: int) -> List[List[T]]:
+    """Разбивает последовательность на списки длиной n."""
+    buf, result = [], []
+    for item in iterable:
+        buf.append(item)
+        if len(buf) == n:
+            result.append(buf)
+            buf = []
+    if buf:
+        result.append(buf)
+    return result
