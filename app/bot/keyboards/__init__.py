2317f10b98932f0ce5aaba74bf5f06f9a51053c6
todmiv <115306605+todmiv@users.noreply.github.com>
2025-07-13 12:48:09 +0300

    Update __init__.py

--- app/bot/keyboards/__init__.py
+++ app/bot/keyboards/__init__.py
@@ -1,1 +1,24 @@
+"""Клавиатуры для телеграм-бота."""
+from .inline_keyboards import (
+    get_main_menu_keyboard,
+    get_profile_keyboard,
+    get_membership_keyboard,
+    get_documents_keyboard,
+    get_consultation_keyboard
+)
+from .reply_keyboards import (
+    get_main_reply_keyboard,
+    get_member_reply_keyboard,
+    get_admin_reply_keyboard
+)
 
+__all__ = [
+    "get_main_menu_keyboard",
+    "get_profile_keyboard", 
+    "get_membership_keyboard",
+    "get_documents_keyboard",
+    "get_consultation_keyboard",
+    "get_main_reply_keyboard",
+    "get_member_reply_keyboard",
+    "get_admin_reply_keyboard"
+]
