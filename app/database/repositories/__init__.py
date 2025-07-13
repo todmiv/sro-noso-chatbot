7296cdc61e16793558a01910b60db03f3627701a
todmiv <115306605+todmiv@users.noreply.github.com>
2025-07-13 14:25:23 +0300

    Update __init__.py

--- app/database/repositories/__init__.py
+++ app/database/repositories/__init__.py
@@ -1,1 +1,12 @@
+"""Репозитории для работы с данными."""
+from .user_repository import UserRepository
+from .message_repository import MessageRepository
+from .document_repository import DocumentRepository
+from .session_repository import SessionRepository
 
+__all__ = [
+    "UserRepository",
+    "MessageRepository",
+    "DocumentRepository", 
+    "SessionRepository"
+]
