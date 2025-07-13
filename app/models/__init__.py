43d524bb2cab78de3632335bc094ff9ceb9cc76c
todmiv <115306605+todmiv@users.noreply.github.com>
2025-07-13 14:20:09 +0300

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,16 @@
+"""ORM модели для приложения."""
+from .base import Base
+from .user import User
+from .message import Message
+from .document import Document
+from .session import Session
+from .feedback import Feedback
 
+__all__ = [
+    "Base",
+    "User",
+    "Message", 
+    "Document",
+    "Session",
+    "Feedback"
+]
